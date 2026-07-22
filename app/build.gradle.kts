88fc99786ca4f4b204e8eb7d9fea2290b0e55896
Mir Zulkifal <73393109+mirtabish@users.noreply.github.com>
2026-07-22 16:19:09 +0530

    Bump versionCode to 7 and versionName to 1.6
    Bump versionCode to 7 and versionName to 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.printlayout.ghxyza"
     minSdk = 24
     targetSdk = 36
-    versionCode = 6
-    versionName = "1.5"
+    versionCode = 7
+    versionName = "1.6"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
