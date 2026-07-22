78d5577ef89d1eded119f7ac41e2601bd00fb43d
Mir Zulkifal <73393109+mirtabish@users.noreply.github.com>
2026-07-22 14:16:59 +0530

    build: increment version code to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     applicationId = "com.aistudio.printlayout.ghxyza"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 3
     versionName = "1.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
